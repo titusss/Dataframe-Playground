367e6a13c8369fc2f684eac1046461a20e87bb31
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-04-06 11:18:27 +0200

    Pre-Server Snapshot

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
+    
     <div>
       <b-button id="show-btn" @click="$bvModal.show('bv-modal-addData')">Add table</b-button>
       <b-modal id="bv-modal-addData" hide-footer>
         <addDataContainer/>
       </b-modal>
+      <baseContainer/>
     </div>
   </div>
 </template>
 
 <script>
 import addDataContainer from './components/addDataContainer.vue'
+import baseContainer from './components/baseContainer'
 
 export default {
   name: 'App',
   components: {
-    addDataContainer
+    addDataContainer,
+    baseContainer
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: SF Compact Rounded, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-family: "SF Compact Rounded", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 .img_in_btn {
   margin-right: 5px;
--- src/components/addDataForm.vue
+++ src/components/addDataForm.vue
@@ -2,98 +2,137 @@
  
  <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">   
-      <b-form-group id="input-group-4">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group-4" label="Type:" label-for="checkboxes-4">
         <b-form-radio-group v-model="form.checked" id="checkboxes-4" required>
-          <b-form-radio name="radio-category" value="RadioCategory">Category</b-form-radio>
-          <div></div>
-          <b-form-radio name="radio-table" value="RadioTable">Table</b-form-radio>
+          <b-form-radio name="table" value="RadioTable">Table</b-form-radio>
+          <b-form-radio name="category" value="RadioCategory">Category</b-form-radio>
         </b-form-radio-group>
       </b-form-group>
       <b-form-group id="input-group-2" label="Title:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder=""
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group
-        id="input-group-1"
-        label="From URL:"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="url"
-          required
-          placeholder=""
-        ></b-form-input>
+        <b-form-input id="input-2" v-model="form.title" required placeholder></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="From Database:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
+      <b-form-group id="input-group-6" label="Source:" label-for="source-card">
+        <b-card no-body id="source-card">
+          <b-tabs card>
+            <b-tab title="Upload" active>
+              <b-form-group
+                id="input-group-4"
+                description="Upload a CSV, TSV, or Excel from your machine."
+              >
+                <b-form-file
+                  v-model="form.source.file"
+                  :state="Boolean(form.source.file)"
+                  placeholder="Choose a file or drop it here..."
+                  drop-placeholder="Drop file here..."
+                  accept=".txt"
+                ></b-form-file>
+                <!-- <div class="mt-3">Selected file: {{ form.source.file ? form.source.file.name : '' }}</div> -->
+              </b-form-group>
+            </b-tab>
+            <b-tab title="Database">
+              <b-form-group
+                id="input-group-3"
+                description="Select a dataset from our global database."
+              >
+                <b-form-select id="input-3" v-model="form.source.database" :options="foods"></b-form-select>
+              </b-form-group>
+            </b-tab>
+            <b-tab title="URL">
+              <b-form-group id="input-group-1" description="Enter a valid URL to your dataset.">
+                <b-form-input id="input-1" v-model="form.source.url" type="url" placeholder></b-form-input>
+              </b-form-group>
+            </b-tab>
+            <b-tab title="Text">
+              <b-form-group id="input-group-1" description="Paste comma-seperated values here.">
+                <b-form-textarea
+                  id="textarea"
+                  v-model="form.source.text"
+                  placeholder="Comma-seperated values..."
+                  rows="9"
+                  max-rows="10000"
+                ></b-form-textarea>
+              </b-form-group>
+            </b-tab>
+          </b-tabs>
+        </b-card>
       </b-form-group>
-      <b-form-group id="input-group-4" label="Upload File:" label-for="input-4" description="CSV, TSV, or Excel">
-        <b-form-file accept=".csv, .txt, .tsv, .xlsx "></b-form-file>
-      </b-form-group>
-      <b-form-textarea
-      id="textarea"
-      v-model="text"
-      placeholder="Or paste comma-seperated values here..."
-      rows="3"
-      max-rows="600"
-    ></b-form-textarea>
-
-    <pre class="mt-3 mb-0">{{ text }}</pre>
+       <b-alert v-model="showErrorAlert" variant="danger" dismissible>{{sourceErrMsg}}</b-alert>
+      <pre class="mt-3 mb-0">{{ text }}</pre>
       <b-button type="submit" variant="primary">Add</b-button>
       <b-button type="reset" variant="danger" @click="$bvModal.hide('bv-modal-addData')">Cancel</b-button>
     </b-form>
-    
   </div>
 </template>
 
 
 <script>
-  export default {
-    name: 'addDataForm',
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-          food: null,
-          checked: []
-        },
-        text: '',
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
+export default {
+  name: "addDataForm",
+  data() {
+    return {
+      sourceErrMsg: "",
+      showErrorAlert: false,
+      form: {
+        title: "",
+        checked: [],
+        source: {
+          file: null,
+          database: null,
+          url: null,
+          text: null
+        }
+      },
+      text: "",
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn"
+      ],
+      show: true
+    };
+  },
+  methods: {
+    validateForm(obj) {
+      var properties = 0;
+      for (var key in obj) {
+        console.log(obj[key])
+        if (obj[key] !== null && obj[key] !== "") {
+          properties++;
+        }
+      }
+      if (properties < 1) {
+        this.sourceErrMsg = "Please enter a data-source.";
+        this.showErrorAlert=true;
+      }
+      else if (properties > 1) {
+        this.sourceErrMsg = "Please enter no more than one data-source.";
+        this.showErrorAlert=true;
+      }
+      else {
+        alert(JSON.stringify(this.form));
       }
     },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.validateForm(this.form.source);
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
+};
 </script>
