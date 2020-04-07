bbb249fd814c187e817c91d49e1bec3dcad2cc2a
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-04-07 20:10:32 +0200

    Added flask server and sceleton backend

--- src/components/matrix.vue
+++ src/components/matrix.vue
@@ -2,36 +2,67 @@
 To-Do: Replace this with a method that generates a single SVG (D3.js) for better performance -->
 
 <template>
-<div>
-  <div class="table_preview">
-    <svg v-for="matrix in matrices" :key="matrix.id" @click="selected = matrix.id, select_matrix(matrix)" :class="{hot:selected==matrix.id, active:selected==matrix.id}" :style="{'grid-area': `${matrix.y} / ${matrix.x} / auto / auto`, 'height': `${(matrix.height * gap)-(gap-rect_height)}px`, 'width': `${(matrix.width * gap)-(gap-rect_width)}px`}" v-b-tooltip :title=matrix.title trigger="click">
-      <g v-for="column in matrix.height" :key="column" class="rows" :style="{transform: `translate(0px, ${column * gap - gap}px)`}" v-bind:class="{ active: matrix.isActive }">
-        <rect v-for="row in matrix.width" :key="row" class="cells" :style="{transform: `translate(${row * gap - gap}px, 0px)`, width: rect_width, height: rect_height}"></rect>
-      </g>
-    </svg>
+  <div>
+    <div class="table_preview">
+      <svg
+        v-for="matrix in matrices"
+        :key="matrix.id"
+        @click="selected = matrix.id, select_matrix(matrix)"
+        :class="{hot:selected==matrix.id, active:selected==matrix.id}"
+        :style="{'grid-area': `${matrix.y} / ${matrix.x} / auto / auto`, 'height': `${(matrix.height * gap)-(gap-rect_height)}px`, 'width': `${(matrix.width * gap)-(gap-rect_width)}px`}"
+        v-b-tooltip
+        :title="matrix.title"
+        trigger="click"
+      >
+        <g
+          v-for="column in matrix.height"
+          :key="column"
+          class="rows"
+          :style="{transform: `translate(0px, ${column * gap - gap}px)`}"
+          v-bind:class="{ active: matrix.isActive }"
+        >
+          <rect
+            v-for="row in matrix.width"
+            :key="row"
+            class="cells"
+            :style="{transform: `translate(${row * gap - gap}px, 0px)`, width: rect_width, height: rect_height}"
+          />
+        </g>
+      </svg>
     </div>
     <div class="table_buttons">
       <b-button-group>
-        <b-button v-b-toggle.collapse-differential v-b-popover.hover.top="'Merges uploaded data with selected table for differential expression when selected.'" title="Differential Expression" :pressed.sync="is_differential" :disabled="button_enabled == 0" size="sm" variant="secondary"><img src="../assets/differential.svg" class="img_in_btn">Differential</b-button>
-        <b-button v-b-modal.modal_delete :disabled="button_enabled == 0" size="sm" variant="danger"><img src="../assets/trash.svg" class="img_in_btn">Remove</b-button>
+        <b-button
+          v-b-toggle.collapse-differential
+          v-b-popover.hover.top="'Merges uploaded data with selected table for differential expression when selected.'"
+          title="Differential Expression"
+          :pressed.sync="is_differential"
+          :disabled="button_enabled == 0"
+          size="sm"
+          variant="secondary"
+        >
+          <img src="../assets/differential.svg" class="img_in_btn" />Differential
+        </b-button>
+        <b-button v-b-modal.modal_delete @click="on_delete_matrix(selected)" :disabled="button_enabled == 0" size="sm" variant="danger">
+          <img src="../assets/trash.svg" class="img_in_btn" />Remove
+        </b-button>
       </b-button-group>
 
-            <b-collapse id="collapse-differential" class="mt-2">
-         <b-card>
+      <b-collapse id="collapse-differential" class="mt-2">
+        <b-card>
           <p class="card-text">Select base column for normalization.</p>
-        <div class="">
-          <b-form>
-            <b-form-select
-          id="input-3"
-          v-model="form.normalization_column"
-          :options="columns"
-          required
-        ></b-form-select>
-        </b-form>
-        </div>
-         </b-card>
-        </b-collapse>
-
+          <div class>
+            <b-form>
+              <b-form-select
+                id="input-3"
+                v-model="form.normalization_column"
+                :options="columns"
+                required
+              ></b-form-select>
+            </b-form>
+          </div>
+        </b-card>
+      </b-collapse>
     </div>
   </div>
 </template>
@@ -42,11 +73,11 @@ export default {
   props: {
     gap: Number,
     rect_width: Number,
-    rect_height: Number
+    rect_height: Number,
+    matrices: Array
   },
   data() {
     return {
-      matrices: [],
       active_matrices: [],
       is_hot: false,
       selected: undefined,
@@ -55,51 +86,43 @@ export default {
       form: {
         normalization_column: null
       },
-       columns: [{ text: 'Select One', value: null },"Mouse 1","Mouse 2 Cow","Pig","Chicken Macrophage","SPI2","NO","SPI2","H2O2","SPI2","-Mg2","SPI2"]
-    }
-  },
-  created() {
-    this.fetch_matrices();
+      columns: [
+        { text: "Select One", value: null },
+        "Mouse 1",
+        "Mouse 2 Cow",
+        "Pig",
+        "Chicken Macrophage",
+        "SPI2",
+        "NO",
+        "SPI2",
+        "H2O2",
+        "SPI2",
+        "-Mg2",
+        "SPI2"
+      ]
+    };
   },
   methods: {
-    fetch_matrices() {
-      const url = './matrices.json';
-        fetch(url)
-        .then(data=>{return data.json()})
-        .then(res=>{
-          this.matrices = res
-          this.get_active_matrices(this.matrices);
-        })
-    },
     select_matrix(matrix) {
-      if (Object.prototype.hasOwnProperty.call(matrix, 'title')) {
+      if (Object.prototype.hasOwnProperty.call(matrix, "title")) {
         this.button_enabled = true;
-      }
-      else {
+
+      } else {
         this.button_enabled = false;
       }
-      console.log(this.button_enabled);
     },
-    get_active_matrices(matrices) {
-      // Unneeded currently, remove in production
-      var i = 0;
-      for(i=0;i<matrices.length;i++) {
-        if (Object.prototype.hasOwnProperty.call(this.matrices[i], 'title')) {
-          this.active_matrices.push(this.matrices[i]);
-        }
-      }
-      console.log(this.active_matrices);
+    on_delete_matrix(deleted_matrix_id) {
+      this.$emit('delete', deleted_matrix_id);
     }
   }
 };
 </script>
 
 <style>
-
 :root {
   --cell-width: 16px;
   --cell-height: 16px;
-  --cell-color: #E0E0E0;
+  --cell-color: #e0e0e0;
 }
 rect {
   fill: var(--cell-color);
@@ -109,7 +132,8 @@ rect {
   display: flex;
 }
 
-.active, svg:hover {
+.active,
+svg:hover {
   --cell-color: #484848;
 }
 
@@ -138,9 +162,14 @@ svg {
 }
 
 @keyframes pulse {
-  0% {opacity: 100%;}
-  50% {opacity: 15%;}
-  100% {opacity: 100%;}
+  0% {
+    opacity: 100%;
+  }
+  50% {
+    opacity: 15%;
+  }
+  100% {
+    opacity: 100%;
+  }
 }
-
 </style>
