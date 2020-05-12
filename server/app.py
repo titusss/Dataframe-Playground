890450ed1d4a1747b877b360621f3a83c7b4d55d
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-05-12 14:09:47 +0200

    Pre-MongoDB

--- server/app.py
+++ server/app.py
@@ -11,10 +11,25 @@ import json
 import load_file
 import visualize
 
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'txt', 'xlsx', 'csv'}
+UPLOAD_FOLDER = '/static' # Change this to /uploads in production
+ALLOWED_EXTENSIONS_MATRIX = {'txt', 'xlsx', 'csv'}
+ALLOWED_EXTENSIONS_ICON = {'svg', 'png', 'jpg', 'jpeg', 'gif'}
 vis_plugin = "clustergrammer"
 VIS_PATH = [""]
+PLUGINS = [
+    {
+        "url": "",
+        "name": "SandDance", 
+        "desc": "Microsoft's 2D & 3D data exploration tool.",
+        "icon": "sanddance_logo.svg"
+    },
+    {
+        "url": "http://amp.pharm.mssm.edu/clustergrammer/matrix_upload/", 
+        "name": "Clustergrammer", 
+        "desc": "Clustering Heatmap by Ma'ayan Laboratory", 
+        "icon": "clustergrammer_preview.svg"
+    }
+]
 MATRIX = [
     {
         "id": uuid.uuid4().hex,
@@ -35,9 +50,9 @@ app.secret_key = "super secret key"  # INSECURE AND FOR DEBUGGING PURPOSES
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app, resources={r'/*': {'origins': '*'}}) # enable CORS
 
-def allowed_file(filename):
+def allowed_file(filename, extension_whitelist):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in extension_whitelist
 
 def remove_matrix(matrix_id):
     for matrix in MATRIX:
@@ -46,40 +61,61 @@ def remove_matrix(matrix_id):
             return True
     return False
 
+@app.route('/plugins', methods=['GET', 'POST'])
+def add_plugin():
+    if request.method == 'POST':
+        file, dataList, extension, filepath, filename = upload_file(request, ALLOWED_EXTENSIONS_ICON, "/Users/titusebbecke/Documents/Work/Helmholtz/2020/Experiments/2003_Hiri_VueBootstrap/hzi_vis_03/public/src/assets")
+        dataList['icon'] = filename # Change this: Deliver static image from webserver not from flask
+        PLUGINS.append(dataList)
+        print("PLUGINS: ", PLUGINS)
+        return jsonify(respond_data('plugin_list', PLUGINS))
+    elif request.method == 'GET':
+        print('method: GET: ', PLUGINS)
+        return jsonify(respond_data('plugin_list', PLUGINS))
+
+
 @app.route('/visualization', methods=['GET', 'POST'])
 def vis_link():
-    vis_response_object = {'status': 'success'}
-    vis_response_object['vis_link'] = VIS_PATH[0]
-    print("VIS_PATH: ",VIS_PATH)
-    print("vis_response_object: ", vis_response_object)
-    return jsonify(vis_response_object)
+    print("vis get")
+    return jsonify(respond_data('vis_link', VIS_PATH[0]))
 
 @app.route('/matrix', methods=['GET', 'POST'])
 def all_matrix():
-    response_object = {'status': 'success'}
-    response_object['matrix'] = MATRIX
-    return jsonify(response_object)
+    return jsonify(respond_data('matrix', MATRIX))
     
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+def add_matrix():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        dataList = json.loads(request.form['form'])
-        # If user does not select file, browser also submit an empty part without filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            extension = os.path.splitext(file.filename)[1]
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            DATAFRAME = make_preview(file, extension, dataList, False) # Main API point. Uploaded dataframe.
-            make_vis_link(vis_plugin, DATAFRAME) # Main API point. Parse Dataframe to whichever visualization plugin you want.
-            return "success"
+        file, dataList, extension, filepath, filename = upload_file(request, ALLOWED_EXTENSIONS_MATRIX, '.' + app.config['UPLOAD_FOLDER'])
+        DATAFRAME = make_preview(file, extension, dataList, False) # Main API point. Uploaded dataframe.
+        make_vis_link(vis_plugin, DATAFRAME) # Main API point. Parse Dataframe to whichever visualization plugin you want.
+        print("responding with: ", jsonify(respond_data('vis_link', VIS_PATH[0])))
+        return jsonify(respond_data('vis_link', VIS_PATH[0]))
+    else: 
+        return "method unclear"
+
+def respond_data(label, payload):
+    response_object = {'status': 'success'}
+    response_object[label] = payload
+    return response_object
+
+def upload_file(request, extension_whitelist, file_dir):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    dataList = json.loads(request.form['form'])
+    # If user does not select file, browser also submit an empty part without filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename, extension_whitelist):
+        extension = os.path.splitext(file.filename)[1]
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(file_dir, filename)
+        file.save(filepath)
+    return file, dataList, extension, filepath, filename
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
@@ -101,9 +137,7 @@ def make_preview(input_file, extension, dataList, remove_id):
     MATRIX.clear()
     MATRICES, DATAFRAME = load_file.process_upload(input_file, extension.lower(), dataList, remove_id)
     for i in range(len(MATRICES)):
-        print(MATRICES[i])
         MATRIX.append(MATRICES[i])
-    print(MATRIX)
     return DATAFRAME
 
 def make_vis_link(vis_plugin, DATAFRAME):
--- server/plugins/clustergrammer.py
+++ server/plugins/clustergrammer.py
@@ -1,19 +1,20 @@
 def main(df):
     file_path = csv_to_tsv(df, 5) # Define the path to the file you want to visualize.
     upload_url = "http://amp.pharm.mssm.edu/clustergrammer/matrix_upload/" # Define the path to the visualizing sertver endpoint.
-    print(upload_url, file_path)
     return upload_url, file_path
 
 def csv_to_tsv(df, cat_amount):
     import pandas as pd
+    import copy
+    dataframe = copy.deepcopy(df)
     file_path = "uploads/output_matrix.txt"
     # Append category title string before values for all cat columns.
-    df[df.columns[0:cat_amount]] = df.columns[0:cat_amount] + \
-        ': ' + df[df.columns[0:cat_amount]].astype(str)
+    dataframe[dataframe.columns[0:cat_amount]] = dataframe.columns[0:cat_amount] + \
+        ': ' + dataframe[dataframe.columns[0:cat_amount]].astype(str)
     # Remove the category titles from first row.
     for i in range(cat_amount):
-        df = df.rename(columns={df.columns[i]: ''})
+        dataframe = dataframe.rename(columns={dataframe.columns[i]: ''})
     # Export the data frame as tab-seperated .txt.
-    df.to_csv(file_path, sep='\t', index=False)
+    dataframe.to_csv(file_path, sep='\t', index=False)
     print('Output file has been generated and saved.')
     return file_path
