1f4396e7515851360dd851048e8108dc6778a2b8
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-05-13 16:12:35 +0200

    Initial MongoDB, Redirecting

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
