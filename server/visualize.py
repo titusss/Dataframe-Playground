095bb7e8a6d1ef83e8ddfe130980fa922132c3df
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-05-18 19:22:58 +0200

    New Matrix Preview and Removal Managament. Added pasted TSV support.

--- server/visualize.py
+++ server/visualize.py
@@ -1,11 +1,11 @@
 # This is the main point for visualizations.
 # Parse all relevant dataframes to this module and decide what plugin to use with route().
 
-def route(vis_plugin, df):
+def route(vis_plugin, df, cat_amount):
     import importlib
     import requests
     vis = importlib.import_module("plugins.{}".format(vis_plugin))
-    upload_url, file_path = vis.main(df)
+    upload_url, file_path = vis.main(df, cat_amount)
     response = requests.post(upload_url, files={'file': open(file_path, 'rb')})
     print(response.text)
     vis_link = response.text
