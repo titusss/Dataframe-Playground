ca024d2d42e1890d7a26935be7f62bf2b2cbc1a1
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-06-16 17:47:47 +0200

    Filter Queries Frontend.

---
+++ src/components/query_blocks/or.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-form-select
+      id="or"
+      size="sm"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      :options="[{text: 'or'}, 'and']"
+      :value="null"
+    ></b-form-select>
+  </div>
+</template>
+    
+<style scoped>
+label {
+    display: inline-flex !important;
+    margin-right: 0.5rem;
+    font-size: 0.875rem;
+}
+</style>
