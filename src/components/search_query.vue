2e8bb1e14a283d033364ad38a0b3949e8b571d4d
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-06-18 19:31:50 +0200

    New Filter Query structure. Connected queries with backend

--- src/components/search_query.vue
+++ src/components/search_query.vue
@@ -1,18 +1,50 @@
 <template>
   <div>
-    <b-form inline>
-      <div v-for="block in blocks" v-bind:key="block.id" class="block-wrapper">
+    <b-form @submit="onSubmit" inline>
+      <b-card
+        bg-variant="light"
+        v-for="form_block in query"
+        v-bind:key="form_block"
+        class="block-wrapper"
+      >
+        <div class="form-block" v-for="form in form_block" v-bind:key="form.id">
+          <label v-if="form.label" :for="form.id">{{form.label}}</label>
+          <b-form-select
+            v-if="form.type === 'b-form-select'"
+            :options="form.options"
+            :value="null"
+            :id="form.id"
+            v-model="form.selected"
+            size="sm"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            required
+          ></b-form-select>
+          <b-form-input
+            v-if="form.type === 'b-form-input'"
+            :id="form.id"
+            v-model="form.selected"
+            size="sm"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            required
+          ></b-form-input>
+        </div>
+        <b-button size="sm" variant="link" v-on:click="remove_query_block(form_block.id)">
+          <b-icon icon="trash"></b-icon>
+        </b-button>
+      </b-card>
+
+      <!-- <div v-for="block in blocks" v-bind:key="block.id" class="block-wrapper">
         <b-card bg-variant="light">
-          <component class="component-inline" v-bind:is="block.name"></component>
+          <component class="component-inline" v-bind:is="block.name" v-bind.sync="form"></component>
         </b-card>
         <b-button size="sm" variant="link" v-on:click="remove_query_block(block.id)">
           <b-icon icon="trash"></b-icon>
         </b-button>
-      </div>
-
+      </div>-->
       <b-dropdown
         size="sm"
         variant="link"
+        pill
         id="add-dropdown"
         text="Add..."
         class="m-md-2 rounded"
@@ -20,8 +52,7 @@
         toggle-class="text-decoration-none"
       >
         <template v-slot:button-content>
-          <b-icon icon="plus-circle-fill"></b-icon>
-          <span class="sr-only">Search</span>
+          <b-icon icon="plus-circle"></b-icon> Add
         </template>
         <b-dropdown-item v-on:click="add_query_block('all_values')">All values...</b-dropdown-item>
         <b-dropdown-item v-on:click="add_query_block('values_in_column')">Values in column...</b-dropdown-item>
@@ -30,44 +61,172 @@
         <b-dropdown-item v-on:click="add_query_block('and')">and</b-dropdown-item>
         <b-dropdown-item v-on:click="add_query_block('or')">or</b-dropdown-item>
       </b-dropdown>
+      <div class="submit-button-parent">
+      <b-button
+        type="submit"
+        variant="primary"
+        pill
+        size="sm"
+        class="submit-button"
+      >
+        <b-icon icon="search"></b-icon> Filter Data
+      </b-button>
+      </div>
     </b-form>
   </div>
 </template>
 
 <script>
-import values_in_column from "@/components/query_blocks/values_in_column.vue";
-import and from "@/components/query_blocks/and.vue";
-import or from "@/components/query_blocks/or.vue";
-import values_in_row from "@/components/query_blocks/values_in_row.vue";
-import values_in_dataframe from "@/components/query_blocks/values_in_dataframe.vue";
-import all_values from "@/components/query_blocks/all_values.vue";
+import axios from "axios";
 export default {
   name: "search_query",
-  components: {
-    values_in_column,
-    and,
-    or,
-    values_in_row,
-    all_values,
-    values_in_dataframe
-  },
   methods: {
     add_query_block(block) {
-      this.blocks.push({ name: block, id: this.id });
+      let added_block = this.form_blocks[block];
+      added_block["id"] = this.id;
+      this.query.push(added_block);
+      console.log(this.query);
       this.id++;
-      console.log(this.blocks);
     },
     remove_query_block(block_id) {
-      this.blocks.splice(
-        this.blocks.findIndex(function(i) {
-          return i.id === block_id;
-        }), 1);
+      this.query.splice(
+        this.query.findIndex(item => item.id === block_id),
+        1
+      );
+    },
+    post_query() {
+      const path = "http://0.0.0.0:5000/query";
+      var data = new FormData();
+      data.append("query", JSON.stringify(this.query));
+      let self = this;
+      // self.$parent.$bvModal.hide('bv_modal_addData')
+      axios
+        .post(path, data)
+        .then(res => {
+          this.$nextTick(() => {
+            console.log("after next tick res: ", res);
+            console.log(JSON.stringify(res));
+            self.$emit("dataframe_change", res);
+            this.show_loading_overlay = false;
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.post_query();
     }
   },
   data() {
     return {
-      blocks: [],
-      id: 0
+      id: 0,
+      query_form_data: {},
+      test: true,
+      form_blocks: {
+        all_values: {
+          logical_operator: {
+            label: "Values are",
+            type: "b-form-select",
+            options: [
+              "< less than",
+              ">= more or equal to",
+              "<= less or equal to",
+              "= equal to",
+              "!= not"
+            ],
+            id: "all_values_logical-operator",
+            selected: null
+          },
+          value: {
+            type: "b-form-input",
+            id: "all_values_value",
+            selected: null
+          }
+        },
+        and: {
+          and_form: {
+            options: [{ text: "and" }, "or"],
+            type: "b-form-select",
+            id: "and",
+            selected: null
+          }
+        },
+        or: {
+          or_form: {
+            options: [{ text: "or" }, "and"],
+            type: "b-form-select",
+            id: "or",
+            selected: null
+          }
+        },
+        values_in_column: {
+          column: {
+            label: "Values in column",
+            type: "b-form-select",
+            options: [
+              { text: "SP Tex (Biological replicate 1)" },
+              "One",
+              "Two",
+              "Three"
+            ],
+            id: "values_in_column_column",
+            selected: null
+          },
+          logical_operator: {
+            label: "are",
+            type: "b-form-select",
+            options: [
+              "< less than",
+              ">= more or equal to",
+              "<= less or equal to",
+              "= equal to",
+              "!= not"
+            ],
+            id: "values_in_column_logical-operator",
+            selected: null
+          },
+          value: {
+            type: "b-form-input",
+            id: "values_in_column_value",
+            selected: null
+          }
+        },
+        values_in_row: {
+          row: {
+            label: "Values in row",
+            type: "b-form-select",
+            options: [
+              { text: "SP Tex (Biological replicate 1)" },
+              "One",
+              "Two",
+              "Three"
+            ],
+            id: "values_in_row_row",
+            selected: null
+          },
+          logical_operator: {
+            label: "are",
+            type: "b-form-select",
+            options: [
+              "< less than",
+              ">= more or equal to",
+              "<= less or equal to",
+              "= equal to",
+              "!= not"
+            ],
+            id: "values_in_row_logical-operator",
+            selected: null
+          },
+          value: {
+            type: "b-form-input",
+            id: "values_in_row_value",
+            selected: null
+          }
+        }
+      },
+      query: []
     };
   }
 };
@@ -78,8 +237,10 @@ label {
   margin-right: 0.5rem;
   font-size: 0.875rem;
 }
-svg {
+svg,
+button {
   margin: 0;
+  text-decoration: none !important;
 }
 .block-wrapper {
   width: 100%;
@@ -93,5 +254,12 @@ svg {
 .card {
   margin-bottom: 0.5rem;
   display: inline-block;
+}
+.form-block {
+  display: inline-flex;
+}
+.submit-button-parent {
+  flex: 1;
+  text-align: right;
 }
 </style>
