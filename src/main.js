b74abdf7eb5c46e8c3a83b945773bb50b8165e17
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-06-26 15:02:38 +0200

    Added initial predictive gene go annotation search (e.coli)

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead)
 
 Vue.config.productionTip = false
 
