7a78dffff0dcbf71bc550fd099439facc3be92de
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-08-03 16:45:09 +0200

    Initial docker backend commit.

---
+++ plugins/Clustergrammer.py
@@ -0,0 +1,25 @@
+def main(df, cat_amount):
+    import requests
+    file_path = csv_to_tsv(df, cat_amount) # Define the path to the file you want to visualize.
+    upload_url = "http://amp.pharm.mssm.edu/clustergrammer/matrix_upload/" # Define the path to the visualizing sertver endpoint.
+    response = requests.post(upload_url, files={'file': open(file_path, 'rb')})
+    print(response.text)
+    vis_link = response.text
+    return vis_link
+
+def csv_to_tsv(df, cat_amount):
+    import pandas as pd
+    import copy
+    dataframe = copy.deepcopy(df)
+    file_path = "uploads/output_matrix.txt"
+    print('cat_aount: ', cat_amount)
+    # Append category title string before values for all cat columns.
+    # dataframe[dataframe.columns[0:cat_amount]] = dataframe.columns[0:cat_amount] + \
+    #     ': ' + dataframe[dataframe.columns[0:cat_amount]].astype(str)
+    # Remove the category titles from first row.
+    for i in range(cat_amount):
+        dataframe = dataframe.rename(columns={dataframe.columns[i]: ''})
+    # Export the data frame as tab-seperated .txt.
+    dataframe.to_csv(file_path, sep='\t', index=False)
+    print('Output file has been generated and saved.')
+    return file_path
---
+++ visualize.py
@@ -0,0 +1,15 @@
+# This is the main point for visualizations.
+# Parse all relevant dataframes to this module and decide what plugin to use with route().
+
+def route(collection, df, cat_amount, plugin):
+    import importlib
+    from pymongo import MongoClient
+    from bson.json_util import ObjectId
+    print(plugin)
+    plugin_module = importlib.import_module("plugins.{}".format(plugin['name']))
+    visualization = {}
+    visualization['plugin_name'] = plugin['name']
+    visualization['plugin_id'] = str(plugin['_id']['$oid'])
+    visualization['link'] = plugin_module.main(df, cat_amount)
+    print('vis_links: ', visualization)
+    return visualization
