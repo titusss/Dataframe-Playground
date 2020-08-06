7ab415665612df0f6f86eccf13f10b86d9889c29
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-08-06 18:19:00 +0200

    Changed Cufflinks visualization. Changed Clustergrammer formatting. Visualizations always prefer filtered dataframe.

--- plugins/Cufflinks.py
+++ plugins/Cufflinks.py
@@ -1,11 +1,14 @@
-def main(df, cat_amount):
+def main(df):
     import uuid
     import cufflinks as cf
     import pandas as pd
-    if cat_amount>0:
-        df.set_index(list(df.columns[:cat_amount]), inplace=True)
+    import numpy as np
+    categories = list(df.select_dtypes(exclude=[np.number]).columns) # Get all columns that are non numeric
+    df.set_index(categories, inplace=True)
+
     print(df)
-    upload_url = df.iplot(kind='heatmap', filename=str(uuid.uuid4()), asUrl=True, colorscale='RdBu')
+    # upload_url = df.iplot(kind='heatmap', filename=str(uuid.uuid4()), asUrl=True, colorscale='RdBu')
+    upload_url = df.iplot(kind='bar', barmode='stack', filename=str(uuid.uuid4()), asUrl=True, colorscale='RdBu')
     print(upload_url)
 
 
