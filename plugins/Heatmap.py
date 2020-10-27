c10a0a4822642878a77f995fc1171a924e53c128
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-10-27 17:56:38 +0100

    Removed unwanted response line.

--- plugins/Heatmap.py
+++ plugins/Heatmap.py
@@ -1,4 +1,3 @@
 def main(parameters):
     upload_url = "https://hp-heatmap-frontend-44nub6ij6q-ez.a.run.app/"
-    response = requests.post(upload_url, files={'file': output})
     return upload_url+parameters["db_entry_id"]
