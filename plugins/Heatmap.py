cbae98e59f42d5e106a34318ce5f6d33e9ccddb4
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-11-11 13:10:08 +0100

    Changed Heatmap plugin URL to fedcloud-helmholtz-cloud.

--- plugins/Heatmap.py
+++ plugins/Heatmap.py
@@ -1,3 +1,4 @@
 def main(parameters):
-    upload_url = "https://hp-heatmap-frontend-44nub6ij6q-ez.a.run.app/"
+    # upload_url = "https://hp-heatmap-frontend-44nub6ij6q-ez.a.run.app/"
+    upload_url = "https://hiri-heatmap.test.fedcloud.eu/"
     return upload_url+'?config='+str(parameters["db_entry_id"])
