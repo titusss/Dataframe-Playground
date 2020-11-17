f9e0e1cfed98cf31350854661b25d355ed2bcd40
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-11-17 01:15:01 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -192,7 +192,8 @@ export default {
   },
   data() {
     return {
-      backend_url: 'https://hiri-webtool-backend-v011-44nub6ij6q-ez.a.run.app',
+      // backend_url: 'https://hiri-webtool-backend-v011-44nub6ij6q-ez.a.run.app',
+      backend_url: http://hiri-heatmap-backend.test.fedcloud.eu,
       // backend_url: 'http://localhost:5000',
       organisms,
       loading: {
