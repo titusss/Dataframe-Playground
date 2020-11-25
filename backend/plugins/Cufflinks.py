9e0f8dc1f1ef1567f7bc1448f2bf3f607309045b
Titus Ebbecke <26855197+chinapwn@users.noreply.github.com>
2020-11-25 19:55:10 +0100

    Fixed Plotly/Cufflinks Username.

--- backend/plugins/Cufflinks.py
+++ backend/plugins/Cufflinks.py
@@ -6,7 +6,7 @@ def main(parameters):
     import chart_studio
     import os
     df = parameters["df"]
-    chart_studio.tools.set_credentials_file(username='tebbecke', api_key=os.environ.get("CHART_STUDIO_API_KEY"))
+    chart_studio.tools.set_credentials_file(username='titusebbecke', api_key=os.environ.get("CHART_STUDIO_API_KEY"))
     categories = list(df.select_dtypes(exclude=[np.number]).columns) # Get all columns that are non numeric
     df.set_index(categories, inplace=True)
     # upload_url = df.iplot(kind='heatmap', filename=str(uuid.uuid4()), asUrl=True, colorscale='RdBu')
